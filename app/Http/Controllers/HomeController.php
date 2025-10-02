2954b04e231a56348a6dba2f70ff905c55a41be2
Richelle1215 <richellemendez35@gmail.com>
2025-10-02 16:57:01 +0800

    new

---
+++ app/Http/Controllers/HomeController.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Suppoert\Facades\Auth;
+use App\Models\User;
+
+class HomeController extends Controller
+{
+    public function redirect(){
+        $usertype = auth()->user()->usertype;
+
+        if($usertype=='0'){
+            return view('admin.home');
+        }
+        else{
+            return view('dashboard');
+        }
+    }
+}
